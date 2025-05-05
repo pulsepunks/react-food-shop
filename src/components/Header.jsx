5171e193b8b04b3852655f0b7039b0f5c6b62ca8
pulsepunk <pulsepunk@yandex.ru>
2025-05-06 02:26:04 +0500

    Base

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+export const Header = () => {
+	return (
+		<header>
+			<nav className='green darken-1'>
+				<div className='nav-wrapper container'>
+					<a
+						href='!#'
+						className='brand-logo'
+					>
+						React Fortnite Shop
+					</a>
+					<ul
+						id='nav-mobile'
+						className='right hide-on-med-and-down'
+					>
+						<li>
+							<a href='!#'>Repo</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+	);
+};
---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+export const Main = ({ children }) => {
+	return (
+		<main className='content green lighten-5'>
+			<div className='container'>{children}</div>
+		</main>
+	);
+};
